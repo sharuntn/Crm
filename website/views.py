b529849fbdb90b3868e46bf7d6561d0d393006e4
sharuntn <sharuntn5@gmail.com>
2023-12-14 20:52:28 +0530

    record created

--- website/views.py
+++ website/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import SignUpForm
+from . models import Record
 def home(request):
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username'] 
         password = request.POST['password'] 
@@ -15,7 +17,7 @@ def home(request):
             messages.success(request,"Error while logging")
             return redirect('home')
     else:
-        return render(request,'home.html',{})
+        return render(request,'home.html',{'records': records})
 
 def login_user(request):
     pass
@@ -42,3 +44,11 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+def customer_record(request,pk):
+     if request.user.is_authenticated:
+          customer_record = Record.objects.get(id=pk)
+          return render(request, 'record.html',{'customer_record':customer_record})
+     else:
+          messages.success(request,"you need to log in")
+          return redirect('home')
